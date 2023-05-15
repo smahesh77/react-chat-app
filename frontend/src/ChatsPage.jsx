8b574aed990952d7bbc1deeb33a0b8455e1fe397
smahesh77 <smahesh7736@gmail.com>
2023-05-15 23:42:10 +0530

    connected chat-engine

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,15 @@
-const chatsPage = () => {
-    return <>chats...</>
-}
-export default chatsPage
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
