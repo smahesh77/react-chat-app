7d166c1b6ba5bdddc2ae82513c963fcae67e49dc
smahesh77 <smahesh7736@gmail.com>
2023-05-15 21:23:22 +0530

    connected to chat engine

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+//product = 28bcb244-5615-4350-86d5-d8a7fc8d2b5f
+
+//key = 5c821132-7e7a-4d84-a545-1a5e1fd4646f
+
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, firstName: username},
+        {headers: {"private-key": "5c821132-7e7a-4d84-a545-1a5e1fd4646f"}}
+    )
+    return res.status(r.status).json(r.data)
+  } catch (err) {
+    return res.status(err.response.status).json(err.response.data)
+
+  }
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+    console.log("listening to port 3001")
+});
